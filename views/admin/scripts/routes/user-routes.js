e5a8a98909ef89a0186d77b76ae2947bc07f9402
root <ashes.vats@gmail.com>
2016-01-12 18:21:07 +0530

    fixed user-routes.js

--- views/admin/scripts/routes/user-routes.js
+++ views/admin/scripts/routes/user-routes.js
@@ -8,6 +8,52 @@ app.config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function
         .state('user', {
             url:'/user',
             templateUrl: 'views/user/main.html',
+              resolve: {
+                loadMyDirectives:function($ocLazyLoad){
+                    return $ocLazyLoad.load(
+                        {
+                            name:'adminApp',
+                            files:[
+                                'scripts/directives/header/header.js',
+                                'scripts/directives/header/header-notification/header-notification.js',
+                                'scripts/directives/sidebar/sidebar.js',
+                                'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                            ]
+                        }),
+                        $ocLazyLoad.load(
+                            {
+                                name:'toggle-switch',
+                                files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
+                                    "bower_components/angular-toggle-switch/angular-toggle-switch.css"
+                                ]
+                            }),
+                        $ocLazyLoad.load(
+                            {
+                                name:'ngAnimate',
+                                files:['bower_components/angular-animate/angular-animate.js']
+                            })
+                    $ocLazyLoad.load(
+                        {
+                            name:'ngCookies',
+                            files:['bower_components/angular-cookies/angular-cookies.js']
+                        })
+                    $ocLazyLoad.load(
+                        {
+                            name:'ngResource',
+                            files:['bower_components/angular-resource/angular-resource.js']
+                        })
+                    $ocLazyLoad.load(
+                        {
+                            name:'ngSanitize',
+                            files:['bower_components/angular-sanitize/angular-sanitize.js']
+                        })
+                    $ocLazyLoad.load(
+                        {
+                            name:'ngTouch',
+                            files:['bower_components/angular-touch/angular-touch.js']
+                        })
+                }
+            },
             onEnter:function()
             {
 
